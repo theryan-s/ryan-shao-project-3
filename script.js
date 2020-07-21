f1d59b390d96d4d67dab03217c3ba40bb47fc364
Ryan Shao <ryan.shao95@gmail.com>
2020-07-21 19:45:04 -0400

    project-3 setup

---
+++ script.js
@@ -0,0 +1,7 @@
+//Create cover page (include some animation)
+//Create instruction/header for the app section
+//'quiz' section, based off of 6 different questions with 3 buttons each
+//event listeners for each category, limiting only one button to be pressed per category. 
+//add event listener for submit
+//once submission is made, obtain users choice to find results
+//results display on the right side
