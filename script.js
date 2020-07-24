11aecc8be71b8825404f812c4d4f1e9f5d345a0c
Ryan Shao <ryan.shao95@gmail.com>
2020-07-24 18:26:00 -0400

    update to photos and script

--- script.js
+++ script.js
@@ -1,13 +1,393 @@
-//Create cover page (include some animation)
-//Create instruction/header for the app section
-//'quiz' section, based off of 6 different questions with 3 buttons each
-//event listeners for each category, limiting only one button to be pressed per category. 
+const breeds = {
+    //Small
+    small : [ 
+        //Small, maintenance: 0, trainability: 0, activeness: 0
+        
+        {
+            dogbreed: "Chihuahua",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/chihuahua.jpg',
+            alt: 'Chihuahua lying down'
+        },
+        
+        {
+            dogbreed: "Chi-Poo",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/chipoo.jpg',
+            alt: 'Chi-Poo wearing a red dress'
+        },
+
+        //Small, maintenance: 1, trainability: 0, activeness: 0
+        {
+            dogbreed: "Shih Tzu",
+            maintenance: 1,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/shihtzu.jpg',
+            alt: 'Shih Tzu in a bag'
+        },
+
+        {
+            dogbreed: "Japanese Chin",
+            maintenance: 1,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/japchin.jpg',
+            alt: 'Japanese Chin dog on the grass'
+        },
+        //Small, maintenance: 0, trainability: 1, activeness: 0
+
+        {
+            dogbreed: "Papillon",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/papillon.jpg',
+            alt: 'Papillon chilling on a comfy rag'
+        },
+
+        {
+            dogbreed: "Pug",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/pug.jpg',
+            alt: 'Pug lying on floor'
+        },
+
+        //Small, maintenance: 0, trainability: 0, activeness: 1
+
+        {
+            dogbreed: "French Bulldog",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/frenchbulldog.jpg',
+            alt: 'French bulldog glaring impatiently'
+        },
+
+        {
+            dogbreed: "Cavalier King",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/cavalierking.jpg',
+            alt: 'Baby Cavalier King looking up'
+        },
+
+        //Small, maintenance: 1, trainability: 1, activeness: 0
+        {
+            dogbreed: "Bichon Frise",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/bichonfrise.jpg',
+            alt: 'Bichon Frise chilling on the grass field'
+        },
+        
+        {
+            dogbreed: "Coton de Tulear",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/cotondetulear.jpg',
+            alt: 'Coton de Tulear running'
+        },
+
+        //Small, maintenance: 0, trainability: 1, activeness: 1
+
+        {
+            dogbreed: "Beagle",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/beagle.jpg',
+            alt: 'Beagle looking upwards'
+        },
+
+        {
+            dogbreed: "Jack Russel",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/jackrussel.jpg',
+            alt: 'Jack Russel looking at you'
+        },
+
+
+        //Small, maintenance: 1, trainability: 0, activeness: 1
+
+        {
+            dogbreed: "Shiba Inu",
+            maintenance: 1,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/shiba.jpg',
+            alt: 'White Shiba looking to the distance'
+        },
+
+        {
+            dogbreed: "Akita", 
+            maintenance: 1,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/akita.jpg',
+            alt: 'Akita looking innocently'
+        },
+
+        //Small, maintenance: 1, trainability: 1, activeness: 1
+        
+        {
+            dogbreed: "Corgi",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/corgi.jpg',
+            alt: 'KIWI THE CORGI AKA MY DOG'
+        },
+
+        {
+            dogbreed: "Border Terrier",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/borderterrier.jpg',
+            alt: 'Puppy Border Terrier lying on a rock'
+        },
+    ],
+    
+    //Medium to large
+
+    midLarge : [
+
+        //Mid-large, maintenance: 0, trainability: 0, activeness: 0
+        {
+            dogbreed: "Chow Chow",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/chowchow.jpg',
+            alt: 'Chow chow chilling in the snow'
+        },
+
+        {
+            dogbreed: "Tibetan Mastiff",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/tibetanmastiff.jpg',
+            alt: 'Tibetan Mastiff standing'
+
+        },
+
+        //Mid-large, maintenance: 1, trainability: 0, activeness: 0
+
+        {
+            dogbreed: "Bulldog",
+            maintenance: 1,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/bulldog.jpg',
+            alt: 'Bulldog chilling in the leaves'
+        },
+
+        {
+            dogbreed: "Bull Terrier",
+            maintenance: 1,
+            trainability: 0,
+            activeness: 0,
+            url: '../images/bullterrier.jpg',
+            alt: 'Bull Terrier running on a field'
+        },
+
+        //Mid-large, maintenance: 0, trainability: 1, activeness: 0
+        {
+            dogbreed: "Cocker Spaniel",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/cockerspaniel.jpg',
+            alt: 'Cocker spaniel soon to fall asleep'
+        },
+
+        {
+            dogbreed: "Poodle",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/poodle.jpg',
+            alt: 'Poodle sneezing'
+        },
+
+        //Mid-large, maintenance: 0, trainability: 0, activeness: 1
+
+        {
+            dogbreed: "Boxer",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/boxer.jpg',
+            alt: 'Boxer dog smiling'
+        },
+
+        {
+            dogbreed: "Pitbull",
+            maintenance: 0,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/pitbull.jpg',
+            alt: 'Pitbull lying underneath a table'
+        },
+
+        //Mid-large, maintenance: 1, trainability: 1, activeness: 0
+
+        {
+            dogbreed: "Samoyed",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/samoyed.jpg',
+            alt: 'Samoyed sleeping comfortably'
+        },
+
+        {
+            dogbreed: "Bernese Mountain",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 0,
+            url: '../images/bernesemountain.jpg',
+            alt:'Bernese Mountain dog enjoying the breeze on a field'
+        },
+
+        //Mid-large, maintenance: 0, trainability: 1, activeness: 1
+
+        {
+            dogbreed: "Australian Shepherd",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/australianshepherd.jpg',
+            alt: 'Baby Australian Shepherd looking at mom'
+        },
+
+        {
+            dogbreed: "Border Collie",
+            maintenance: 0,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/bordercollie.jpg',
+            alt: 'Border Collie chilling on wooden floor'
+        },
+
+        //Mid-large, maintenance: 1, trainability: 0, activeness: 1
+
+        {
+            dogbreed: "Dalmatian",
+            maintenance: 1,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/dalmatian.jpg',
+            alt: 'Dalmatian chilling on the beach'
+        },
+
+        {
+            dogbreed: "Dobermann",
+            maintenance: 1,
+            trainability: 0,
+            activeness: 1,
+            url: '../images/dobermann.jpg',
+            alt: 'Dobermann chewing on a stick'
+        },
+
+        //Mid-large, maintenance: 1, trainability: 1, activeness: 1
+        {
+            dogbreed: "German Shepherd",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/germanshepherd.jpg',
+            alt: 'German Shepherd chilling'
+        },
+
+        {
+            dogbreed: "Golden Retriever",
+            maintenance: 1,
+            trainability: 1,
+            activeness: 1,
+            url: '../images/goldenretriever.jpg',
+            alt: 'Golden Retriever chilling in a field'
+        },
+        
+    ],
+};
+
+//'quiz' section, based off of 4 different questions with 2 buttons each
+//event listeners for each category
 //add event listener for submit
 //once submission is made, obtain users choice to find results
 //results display on the right side
 
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
+function randomItem(optionsArray) {
+    const index = Math.floor(Math.random() * optionsArray.length);
+    return optionsArray[index];
+}
+
+$(function(){
+
+    //Slow scroll from start to first slide
+    $('.start').on('click', function() {
+        $('html,body').animate({
+            scrollTop: $(".questions").offset().top
+        }, 'slow');
+    });
+
+    $('form').on('submit', function(event){
+        event.preventDefault();
+
+        //Get User's choice and store in variable
+        const userSize = $('input[name=size]:checked').val();
+        const userMaintenance = $('input[name=maintenance]:checked').val();
+        const userTrainability = $('input[name=trainability]:checked').val();
+        const userActiveness = $('input[name=activeness]:checked').val();
+
+        //Filter based off of user's size selection
+        const userChoice = breeds[userSize];
+
+        console.log(userSize);
+        console.log(userMaintenance);
+        console.log(userTrainability);
+        console.log(userActiveness);
+
+        //Exclude empty options
+        if (!userSize || !userMaintenance || !userTrainability || !userActiveness){
+            alert("Please fill out all the questions!");
+            return;
+        };
+
+        //Find match between user selected options with list
+
+        const finalOptions = userChoice.filter((breed) => {
+            return (Number(userMaintenance) === breed.maintenance) && 
+            (Number(userTrainability) === breed.trainability) &&
+            (Number(userActiveness) === breed.activeness);
+        }); 
+
+        //Given the results of objects, randomize a dog breed for user
+        const result = randomItem(finalOptions);
+
+        //Create variables for the image and alt text path
+        const imgURL = result.url;
+        const altText = result.alt;
+        
+        //Display results
+        $('.choice').text(`${result.dogbreed}`);
+        $('.result img').attr({
+            "src": imgURL, 
+            "alt": altText,
+        });
+    });
 });
